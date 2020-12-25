c63973b4326fcfc202a98bed1db91777a67f9d9f
fviktor92 <farkas.viktor92@gmail.com>
2020-12-25 08:38:43 +0100

    added bindings

--- app/src/main/java/com/graphipuzzle/PlayFieldActivity.kt
+++ app/src/main/java/com/graphipuzzle/PlayFieldActivity.kt
@@ -3,15 +3,23 @@ package com.graphipuzzle
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.graphipuzzle.databinding.ActivityPlayFieldBinding
 
 class PlayFieldActivity : AppCompatActivity()
 {
+	private lateinit var binding: ActivityPlayFieldBinding
+
 	override fun onCreate(savedInstanceState: Bundle?)
 	{
 		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_play_field)
-		var textView: TextView = findViewById(R.id.play_field_text)
+		binding = DataBindingUtil.setContentView(this, R.layout.activity_play_field)
+
+		var textView: TextView = binding.playFieldText
 		val playField = PlayField(ReadPlayField(this, "level_1").getFile())
-		textView.text = playField.getFieldRows().toString()
+		binding.apply {
+			invalidateAll() // Refresh the UI with the new data, invalidating all binding expressions so that they get recreated with the correct data
+			textView.text = playField.getFieldRows().toString()
+		}
 	}
 }
